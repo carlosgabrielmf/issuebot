ad1f949606355dcc25bcb64e1519cfbf57a22bba
Ana Hartmann <ana.hartmann@protonmail.com>
2023-06-21 14:37:16 +0200

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
 import { DeveloperFactory } from "./back-end/developer/developer.factory";
+import { useState, FormEvent, createContext} from 'react';
 
+
+interface FormData {
+  name: string;
+  level: string;
+  role: string;
+  skills: string;
+  issues: string;
+}
 const test = () => {};
 
 export default async function Home() {
+
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    level: '',
+    role: '',
+    skills: '',
+    issues: ''
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  
   const medusaTeam = DeveloperFactory.medusaTeam();
   // const data = await main(['api'], 1, medusaTeam);
 
@@ -31,8 +55,37 @@ export default async function Home() {
         </div>
       </div>
       {/* the form is here */}
-      <div>
+      <div className={styles.form}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })}/>
+        <input type="text" placeholder="Level" value={formData.level} onChange={(e) => setFormData({ ...formData, level: e.target.value })}/>
+        <input type="text" placeholder="Role" value={formData.role} onChange={(e) => setFormData({ ...formData, role: e.target.value })}/>
+        <input type="text" placeholder="Skills" value={formData.skills} onChange={(e) => setFormData({ ...formData, skills: e.target.value })}/>
+        <input type="text"placeholder="Issues" value={formData.issues} onChange={(e) => setFormData({ ...formData, issues: e.target.value })}/>
+        <button type="submit">Submit</button>
+      </form>
 
+      {/* Display the table with the form data */}
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Role</th>
+            <th>Skills</th>
+            <th>Issues</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{formData.name}</td>
+            <td>{formData.level}</td>
+            <td>{formData.role}</td>
+            <td>{formData.skills}</td>
+            <td>{formData.issues}</td>
+          </tr>
+        </tbody>
+      </table>
       </div>
     </main>
   );
