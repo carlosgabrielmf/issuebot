22b7c0e1fbb6bc6ca14e0b5e6296620f4374e453
cgabriel <carlosgabrielmosquera@gmail.com>
2023-06-21 23:53:34 +0200

    se anade componente de card iterado

--- src/app/components/open-ai-issues-result.tsx
+++ src/app/components/open-ai-issues-result.tsx
@@ -1,36 +1,40 @@
 "use client";
-import React from "react"
-import Card from '@mui/material/Card';
-import Box from '@mui/material/Box';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { OpenAiResponse } from "../back-end/open-ai/open-ai.response"
+import React from "react";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { OpenAiResponse } from "../back-end/open-ai/open-ai.response";
 
-const OpenAiIssuesResult: React.FC<OpenAiResponse[]> = (result: OpenAiResponse[]) => {
-    return (
-        <div>
-            <Typography variant="h5" component="div">
-                Developer
-            </Typography>
-            <Box>
-                <Card sx={{ minWidth: 275 }}>
-                    <CardContent>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            Titulo issue
-                        </Typography>
-                        <Typography variant="body2">
-                            Lorem ipsum...
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Issue URL</Button>
-                    </CardActions>
-                </Card>
-            </Box>
-        </div>
-    )
-}
+const OpenAiIssuesResult: React.FC<OpenAiResponse[]> = (
+  results: OpenAiResponse[]
+) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <>
+          <Typography variant="h5" component="div">
+            {result.developer_name}
+          </Typography>
+          <Box>
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {result.title}
+                </Typography>
+                <Typography variant="body2">{""}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">{result.url}</Button>
+              </CardActions>
+            </Card>
+          </Box>
+        </>
+      ))}
+    </div>
+  );
+};
 
-export default OpenAiIssuesResult
+export default OpenAiIssuesResult;
