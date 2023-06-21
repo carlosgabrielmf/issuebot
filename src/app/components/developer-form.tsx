6c426564039b49b022d195b789663898a34c1928
cgabriel <carlosgabrielmosquera@gmail.com>
2023-06-21 22:42:56 +0200

    se agregan estilos al proyecto

--- src/app/components/developer-form.tsx
+++ src/app/components/developer-form.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React, { FormEvent, useState } from 'react';
+"use client";
+import React, { FormEvent, useState } from "react";
 import styles from "../page.module.css";
 
 interface FormData {
@@ -13,26 +13,28 @@ interface FormData {
 const DeveloperForm: React.FC = () => {
   const [developers, setDevelopers] = useState<FormData[]>([]);
   const [formData, setFormData] = useState<FormData>({
-    name: '',
-    level: '',
-    role: '',
-    skills: '',
-    issues: '',
+    name: "",
+    level: "",
+    role: "",
+    skills: "",
+    issues: "",
   });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     setDevelopers([...developers, formData]);
     setFormData({
-      name: '',
-      level: '',
-      role: '',
-      skills: '',
-      issues: '',
+      name: "",
+      level: "",
+      role: "",
+      skills: "",
+      issues: "",
     });
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -42,51 +44,92 @@ const DeveloperForm: React.FC = () => {
 
   return (
     <section className={styles.section}>
-    <div className={styles.container}>
-      <form  className={styles.form} onSubmit={handleSubmit}>
-        <label htmlFor="name">Developer Name:</label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+      <div className={styles.container}>
+        <h2
+          style={{
+            fontSize: "1.7rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "1rem",
+          }}
+          className={styles.description}>
+          Find the issues. Good Luck!
+        </h2>
+        <hr style={{color: "rgba(1, 65, 255, 0.3)"}}/>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label htmlFor="name">Developer Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="level">Level:</label>
-        <input type="text" id="level" name="level" value={formData.level} onChange={handleChange} />
+          <label htmlFor="level">Level:</label>
+          <input
+            type="text"
+            id="level"
+            name="level"
+            value={formData.level}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="role">Role:</label>
-        <input type="text" id="role" name="role" value={formData.role} onChange={handleChange} />
+          <label htmlFor="role">Role:</label>
+          <input
+            type="text"
+            id="role"
+            name="role"
+            value={formData.role}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="skills">Skills:</label>
-        <textarea id="skills" name="skills" value={formData.skills} onChange={handleChange} />
+          <label htmlFor="skills">Skills:</label>
+          <textarea
+            id="skills"
+            name="skills"
+            value={formData.skills}
+            onChange={handleChange}
+          />
 
-        <label htmlFor="issues">Issues per Developer:</label>
-        <input type="number" id="issues" name="issues" value={formData.issues} onChange={handleChange} />
+          <label htmlFor="issues">Issues per Developer:</label>
+          <input
+            type="number"
+            id="issues"
+            name="issues"
+            value={formData.issues}
+            onChange={handleChange}
+          />
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
 
-<div className={styles.tablewrapper}>
-      <table className={styles.fltable}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Level</th>
-            <th>Role</th>
-            <th>Skills</th>
-            <th>Issues</th>
-          </tr>
-        </thead>
-        <tbody>
-          {developers.map((developer, index) => (
-            <tr key={index}>
-              <td>{developer.name}</td>
-              <td>{developer.level}</td>
-              <td>{developer.role}</td>
-              <td>{developer.skills}</td>
-              <td>{developer.issues}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-    </div>
+        <div className={styles.tablewrapper}>
+          <table className={styles.fltable}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Level</th>
+                <th>Role</th>
+                <th>Skills</th>
+                <th>Issues</th>
+              </tr>
+            </thead>
+            <tbody>
+              {developers.map((developer, index) => (
+                <tr key={index}>
+                  <td>{developer.name}</td>
+                  <td>{developer.level}</td>
+                  <td>{developer.role}</td>
+                  <td>{developer.skills}</td>
+                  <td>{developer.issues}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </section>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { DeveloperFactory } from "./back-end/developer/developer.factory";
-import React from 'react';
+import React from "react";
 import DeveloperForm from "./components/developer-form";
-
 
 const test = () => {};
 
@@ -28,14 +27,43 @@ const Page: React.FC = () => {
           <Image
             src="/assets/4.png"
             alt="Bot Saying Hello"
-            width={500}
-            height={500}
+            width={300}
+            height={300}
           />
         </div>
       </div>
+      <div className={styles.wrapper}>
+        <div className={styles.description}>
+          <div className="header-image">
+            <Image
+              style={{marginTop: "-1rem"}}
+              src="/assets/5.png"
+              alt="Bot Saying Hello"
+              width={300}
+              height={300}
+            />
+          </div>
+          <h3>How does this work?</h3>
+          <ul className={styles.description}>
+            <li>
+              First, get ready for the best experience of finding an issue, fast
+              and easy.
+            </li>
+            <li>
+              Second, wait and leave the work to the IA. Pour yourself a coffee.
+            </li>
+            <li>
+              Third, fill out the form, wait for the results and have fun.
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Image src="/assets/3.png" alt="Bot Finding" width={350} height={350} />
         <DeveloperForm />
+      </div>
     </main>
   );
-}
+};
 
 export default Page;
